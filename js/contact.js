1dc7a897ea4c63284ef0a39115dde87c001838f7
divyansh09github <divyanshagarwal201@gmail.com>
2022-06-29 02:56:04 +0530

    ddbakers

---
+++ js/contact.js
@@ -0,0 +1,42 @@
+$('document').ready(function(){
+	$('input[type="text"], input[type="email"], textarea').focus(function(){
+		var background = $(this).attr('id');
+		$('#' + background + '-form').addClass('formgroup-active');
+		$('#' + background + '-form').removeClass('formgroup-error');
+	});
+	$('input[type="text"], input[type="email"], textarea').blur(function(){
+		var background = $(this).attr('id');
+		$('#' + background + '-form').removeClass('formgroup-active');
+	});
+
+	// const btn = document.querySelector('.button');
+	// btn.addEventListener('click',(e)=>{
+	// 	e.preventDefault();
+	// });
+
+function errorfield(field){
+	$(field).addClass('formgroup-error');
+	console.log(field);	
+}
+
+$("#waterform").submit(function() {
+	var stopsubmit = false;
+
+if($('#name').val() == "") {
+	errorfield('#name-form');
+	stopsubmit=true;
+}
+if($('#email').val() == "") {
+	errorfield('#email-form');
+	stopsubmit=true;
+}
+  if(stopsubmit) return false;
+});
+		
+});
+
+
+
+// const store = document.querySelector(".nev-item").addEventListener("click",()=>{
+// 	window.scrollTo(0,document.body.scrollHeight);
+// });
